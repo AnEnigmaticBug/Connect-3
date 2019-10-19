cdd4b231b97a7eca4c3d95d4af284cb91ac7a2f9
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-10-19 11:07:32 +0530

    Remove the Human class since it's no longer in use
    It was used only in the earlier(and now non-existent) cli version.

--- human.py
+++
@@ -1,9 +0,0 @@
-# NISHANT MAHAJAN
-# 2017A7PS0112P
-
-class Human:
-    def __init__(self, marker):
-        self.marker = marker
-
-    def action(self, state):
-        return int(input('Your action: '))
